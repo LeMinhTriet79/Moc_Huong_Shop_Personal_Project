0b358201e75d5aa4d13cb1a238b068e56e7fe898
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-07-19 11:47:36 +0700

    up

--- aromatics-platform/catalog-service/src/main/java/com/minhtriet/se3979/catalogservice/entity/Product.java
+++ aromatics-platform/catalog-service/src/main/java/com/minhtriet/se3979/catalogservice/entity/Product.java
@@ -63,6 +63,8 @@ public class Product {
     @Builder.Default
     private Boolean isPublished = false;
 
+
+
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
